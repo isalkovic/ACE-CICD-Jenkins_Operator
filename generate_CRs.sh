cd4d18a3540dcc0f3d68eca500eb938312f03edd
Ivo Salkovic <ivo.salkovic@gmail.com>
2021-12-07 11:52:53 +0100

    fix to script

--- generate_CRs.sh
+++ generate_CRs.sh
@@ -87,8 +87,9 @@ if [ -d "${DIRpolicies}" ]
 then
 	if [ "$(ls -A ${DIRpolicies})" ]; then
     echo "Generating policy CR yaml"
-		tar -cZf ${PathToConfigFolder}/policy.zip -C ${DIRpolicies} .
-    # works if you have zip installed:: zip -j - ${DIRpolicies}/* > ${DIRpolicies}/policy.zip -x '*.zip*'
+		# alternative to zip - requires tar and compress:: tar -cZf ${PathToConfigFolder}/policy.zip -C ${DIRpolicies} .
+    # works if you have zip installed::
+		zip -j - ${DIRpolicies}/* > ${PathToConfigFolder}/policy.zip -x '*.zip*'
     policy=$(base64 -w 0 ${PathToConfigFolder}/policy.zip)
     sed -e "s/replace-with-namespace/${Namespace}/" -e "s~replace-with-policy-name~${appname}-policy~" -e "s~replace-with-policy-base64~${policy}~" ${CRs_template_folder}/configuration_policyProject.yaml > ${CRs_generated_folder}/configurations/policyProject-generated.yaml
     #add reference to this config cr to integration server cr
